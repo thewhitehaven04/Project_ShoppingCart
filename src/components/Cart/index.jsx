3563bc4909740bd4446d26203a1b76a7e6bc969d
German <thewhitehaven04@gmail.com>
2023-07-01 23:33:54 +0200

    empty cart icon added

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -3,6 +3,8 @@ import { useShoppingCart } from 'providers/Cart';
 import React from 'react';
 import style from './../../styles/cart.css';
 import { formatPrice } from 'utils/formatPrice';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 export default function Cart() {
   const cartItems = useShoppingCart();
@@ -10,7 +12,12 @@ export default function Cart() {
   const totalSum = cartItems.reduce((sum, item) => sum + item.price, 0);
 
   return cartItems.length === 0 ? (
-    <span>There are no items in your cart</span>
+    <>
+      <div className="cart__empty cart-icon">
+        <FontAwesomeIcon className="cart-icon_element" icon={faCartShopping} />
+      </div>
+      <span className='cart__empty-text'>There are no items in your cart</span>
+    </>
   ) : (
     <>
       <ul className="cart-items__collection">
